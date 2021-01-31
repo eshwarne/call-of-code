d2837294d969e9a8a03b3809891ef700ee1dd9f4
eshwarne <northeasteditsofficial@gmail.com>
2021-01-31 17:02:10 +0530

    setup postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,16 @@
-console.log("Watched")
+// import {MikroORM} from "@mikro-orm/core"
+import dotenv from "dotenv"
+dotenv.config()
+console.log(process.env.POSTGRES_PASSWORD)
 
+// const startUp = async () =>{
+//      const orm = MikroORM.init({
+//         dbName:"callofcode",
+//         user:process.env.POSTGRES_USER,
+//         password:process.env.POSTGRES_PASSWORD,
+//         debug:true
+//     })
+//     orm
+// }
+
+// startUp()
